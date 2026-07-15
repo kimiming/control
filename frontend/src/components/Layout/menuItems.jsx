46d76aa55469b53183fc182be3553148f9b6be14
kimiming <635596535@qq.com>
2026-07-15 00:14:19 -0700

    fix something

--- frontend/src/components/Layout/menuItems.jsx
+++ frontend/src/components/Layout/menuItems.jsx
@@ -2,12 +2,12 @@ import { AppstoreOutlined, ContactsOutlined, CustomerServiceOutlined, FileTextOu
 
 export const menuItems = [
   { key: '/sessions', icon: <UserOutlined />, label: 'Session管理' },
-  { key: '/usage-docs', icon: <FileTextOutlined />, label: '使用文档' },
   { key: '/messages', icon: <MessageOutlined />, label: '消息列表' },
   { key: '/customers', icon: <CustomerServiceOutlined />, label: '客服管理' },
   { key: '/customer-profiles', icon: <ContactsOutlined />, label: '客户资料管理' },
   { key: '/materials', icon: <AppstoreOutlined />, label: '素材库管理' },
   { key: '/tasks', icon: <ProfileOutlined />, label: '任务管理' },
   { key: '/proxies', icon: <PartitionOutlined />, label: '代理管理' },
+  { key: '/usage-docs', icon: <FileTextOutlined />, label: '使用文档' },
   { key: '/users', icon: <TeamOutlined />, label: '用户管理', rootOnly: true },
 ];
